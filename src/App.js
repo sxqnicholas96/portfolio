354da98fd5e36bd9bb833a57b5948db34009f178
sxqnicholas96 <sxq.nicholas96@gmail.com>
2019-03-29 20:18:52 +0800

    revamped AppBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from "react";
-import { Layout, Navigation, Drawer, Content, Header } from "react-mdl";
-import { Link } from "react-router-dom";
+import { Layout, Content } from "react-mdl";
+import { Link as RouterLink } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 import "./App.css";
 import Main from "./components/main";
+import Avatar from "./images/avatar.png";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <Header title={"Seah Xi Qi, Nicholas"} className="header_color">
+          <AppBar position="static" className="header_color">
+            <Toolbar>
+              <IconButton component={RouterLink} to="/" disableRipple={true}>
+                <img style={{ width: "60px" }} src={Avatar} alt="Avatar" />
+              </IconButton>
+              <Typography style={{ flexGrow: 1 }} variant="h6" color="inherit">
+                Seah Xi Qi, Nicholas
+              </Typography>
+              <Button
+                component={RouterLink}
+                to="/"
+                color="inherit"
+                size="large"
+              >
+                Home
+              </Button>
+              <Button
+                color="inherit"
+                component={RouterLink}
+                to="/resume"
+                size="large"
+              >
+                Resume
+              </Button>
+              <Button
+                color="inherit"
+                component={RouterLink}
+                to="/projects"
+                size="large"
+              >
+                Projects
+              </Button>
+            </Toolbar>
+          </AppBar>
+          {/* <Header title={"Seah Xi Qi, Nicholas"} className="header_color">
             <Navigation>
               <Link to="/">Home</Link>
               <Link to="/resume">Resume</Link>
@@ -23,7 +63,7 @@ class App extends Component {
               <Link to="/resume">Resume</Link>
               <Link to="/projects">Projects</Link>
             </Navigation>
-          </Drawer>
+          </Drawer> */}
 
           <Content>
             <Main />
